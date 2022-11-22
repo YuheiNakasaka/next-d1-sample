2dce2caa5df7c875c9f07aefe1458c987de9c0e7
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2022-11-22 11:47:33 +0900

    ENVにアクセスできるかテスト

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,14 +1,22 @@
 import type { NextRequest } from "next/server";
+import type { Database } from "@cloudflare/d1";
 
 export const config = {
   runtime: "experimental-edge",
 };
 
-export default async function (req: NextRequest) {
-  return new Response(JSON.stringify({ name: "John Doe" }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+export interface Env {
+  DB: Database;
+}
+
+export default async function (req: NextRequest, env: Env) {
+  return new Response(
+    JSON.stringify({ name: "John Doe", env: JSON.stringify(env) }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
