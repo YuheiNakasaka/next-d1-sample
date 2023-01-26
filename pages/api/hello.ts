2accb60a02e61dc953eec6fff2c791b0125aeb84
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-01-26 10:18:25 +0900

    test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,14 @@
 import type { NextRequest } from "next/server";
 import type { Database } from "@cloudflare/d1";
 
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB: Database;
+    }
+  }
+}
+
 export const config = {
   runtime: "experimental-edge",
 };
@@ -11,24 +19,39 @@ export interface Env {
 
 type CfNextRequest = NextRequest & { env: any };
 
-export default async function (req: CfNextRequest, env: Env) {
-  const { results } = await (process.env as any).DB.prepare(
-    "SELECT * FROM Customers WHERE CompanyName = ?"
-  )
-    .bind("Bs Beverages")
-    .all();
-  return new Response(
-    JSON.stringify({
-      req: JSON.stringify(req),
-      env: JSON.stringify(env),
-      results: JSON.stringify(results),
-      processEnv: JSON.stringify(process.env),
-    }),
-    {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+export default async function (req: CfNextRequest) {
+  try {
+    const { results } = await process.env.DB.prepare(
+      "SELECT * FROM Customers WHERE CompanyName = ?"
+    )
+      .bind("Bs Beverages")
+      .all();
+    return new Response(
+      JSON.stringify({
+        req: JSON.stringify(req),
+        results: JSON.stringify(results),
+        processEnv: JSON.stringify(process.env),
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (e: any) {
+    return new Response(
+      JSON.stringify({
+        req: JSON.stringify(req),
+        processEnv: JSON.stringify(process.env),
+        error: e.message,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
